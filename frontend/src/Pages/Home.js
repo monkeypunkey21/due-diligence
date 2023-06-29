43b41bd4cd91b5846fa79bc1c89a6296aef7762f
Abhi M <monkeypunkey79@gmail.com>
2023-06-29 12:28:08 -0700

    Feat: implemented fetchposts logic

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,12 +1,32 @@
 import { Heading } from "@chakra-ui/layout"
+import {useState, useEffect} from 'react'
 
 const Home = () =>
 {
+
+const [posts, setPosts] = useState([])
+
+
+//on initial render
+useEffect(() =>
+{
+    const fetchPosts = async () =>
+    {
+        const response = await fetch('http://localhost:4000/api/posts/')
+        const json = await response.json()
+        setPosts(json)
+    }
+
+    fetchPosts()
+}
+, [])
+
     return (
         <div>
             <Heading>
                 Home
             </Heading>
+
         </div>
     )
 }
