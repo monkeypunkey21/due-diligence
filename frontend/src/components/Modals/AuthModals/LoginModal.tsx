d277b670079d71d93ad2b6ce57efd13329cfd76d
Abhi M <monkeypunkey79@gmail.com>
2023-07-24 15:02:09 -0700

    Feat: Sign Up and Login modals

--- frontend/src/components/Modals/AuthModals/LoginModal.tsx
+++ frontend/src/components/Modals/AuthModals/LoginModal.tsx
@@ -1,10 +1,35 @@
 import useAuthStore from "../../../state/AuthStore";
-import { Modal, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay } from "@chakra-ui/react";
-import React from "react";
+import { FormControl, FormLabel, Input, Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const LoginModal: React.FC = () =>
 {
     const {isLoginOpen, closeLoginModal} = useAuthStore((state) => ({isLoginOpen: state.isLoginOpen, closeLoginModal: state.closeLoginModal}));
+    const [user, setUser] = useState('')
+    const [pass, setPass] = useState('')
+    const [error, setError] = useState('')
+
+
+    const handleSubmit = async () =>
+    {
+
+        if ( !user || !pass)
+            setError('Must fill out all fields')
+
+        const newUser = { 
+            user, 
+            pass
+        }
+        
+        const response = await fetch('http://localhost:4000/api/users/',
+        {
+            method: "POST",
+            body: JSON.stringify(newUser),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
 
     return (
         <div>
@@ -13,6 +38,17 @@ const LoginModal: React.FC = () =>
             <ModalContent>
                 <ModalHeader>Log In</ModalHeader>
                 <ModalCloseButton/>
+                <ModalBody>
+                    <FormControl>
+                        <FormLabel>Username:</FormLabel>
+                        <Input onChange={(e) => {setUser(e.target.value)}} value={user}/>
+                        <FormLabel>Password:</FormLabel>
+                        <Input onChange={(e) => {setPass(e.target.value)}} value={pass}/>
+
+                        <Button onClick={handleSubmit}>Create Account</Button>
+                        <Text>{error}</Text>
+                    </FormControl>
+                </ModalBody>
             </ModalContent>
         </Modal>    
         </div>
--- frontend/src/components/Modals/AuthModals/SignInModal.tsx
+++ frontend/src/components/Modals/AuthModals/SignInModal.tsx
@@ -1,10 +1,46 @@
 import useAuthStore from "../../../state/AuthStore";
-import { Modal, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay } from "@chakra-ui/react";
-import React from "react";
+import { FormControl, FormLabel, Input, Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const SigninModal: React.FC = () =>
 {
     const {isSigninOpen, closeSignInModal} = useAuthStore((state) => ({isSigninOpen: state.isSignInOpen, closeSignInModal: state.closeSignInModal}));
+
+    const [email, setEmail] = useState('')
+    const [user, setUser] = useState('')
+    const [pass, setPass] = useState('')
+    const [error, setError] = useState('')
+    const handleSubmit = async () =>
+    {
+        if (!email || !user || !pass)
+            setError('Must fill out all fields')
+
+        const newUser = {
+            email, 
+            user, 
+            pass
+        }
+        
+        const response = await fetch('http://localhost:4000/api/users/',
+        {
+            method: "POST",
+            body: JSON.stringify(newUser),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const json = await response.json()
+
+        if (response.ok)
+        {
+            setEmail('')
+            setUser('')
+            setPass('')
+        }
+
+    }
+
 
     return (
         <div>
@@ -13,6 +49,19 @@ const SigninModal: React.FC = () =>
             <ModalContent>
                 <ModalHeader>Sign In</ModalHeader>
                 <ModalCloseButton/>
+                <ModalBody>
+                    <FormControl>
+                        <FormLabel>Email:</FormLabel>
+                        <Input onChange={(e) => {setEmail(e.target.value)}} value={email}/>
+                        <FormLabel>Username:</FormLabel>
+                        <Input onChange={(e) => {setUser(e.target.value)}} value={user}/>
+                        <FormLabel>Password:</FormLabel>
+                        <Input onChange={(e) => {setPass(e.target.value)}} value={pass}/>
+
+                        <Button onClick={handleSubmit}>Create Account</Button>
+                        <Text>{error}</Text>
+                    </FormControl>
+                </ModalBody>
             </ModalContent>
         </Modal>    
         </div>
